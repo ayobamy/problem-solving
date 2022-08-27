bf7563e0506facce58a27da5339d3c905ccfc153
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-08-27 02:00:46 +0000

    [feat]: absolute value of a number

---
+++ absolute-value/abs_value.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int num;
+    scanf("%d", &num);
+
+    if (num >= 0)
+    {
+        printf("The absolute value is: %d\n", num);
+    }
+    else {
+        num = num * -1;
+        printf("The absolute value is: %d\n", num);
+    }
+
+    return 0;
+}
