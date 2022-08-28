f43cf4bd8791b9ab36c6054be8171924e1485507
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-08-28 09:55:40 +0000

    [feat]: reverse a number

--- reverse_a_number/reverse.py
+++ reverse_a_number/reverse.py
@@ -1,9 +1,12 @@
 num = int(input())
 
+# num = int(num[::-1])
+# print(f"{num}")
+
 rev = 0
 while (num > 0):
     mod = num % 10
     rev = (rev * 10) + mod
     num //= 10
-    
+
 print(f"{rev}")
