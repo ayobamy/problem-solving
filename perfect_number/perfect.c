31fc5506bc5b8abff77c58c5a47be45d53f5a16f
ayobamy <ayobamyahmed@gmail.com>
2022-09-04 19:53:39 +0000

    [feat]: perfect number

--- perfect_number/perfect.c
+++ perfect_number/perfect.c
@@ -3,10 +3,28 @@
 
 int main()
 {
-    int n;
-
+    int n, mod, sum, i;
+    sum = 0;
+    
     scanf("%d", &n);
-
+    
+    for (i = 1; i < n; i++)
+    {
+        mod = n % i;
+        if (mod == 0)
+        {
+            sum += i;
+        }
+    }
+    
+    if (sum == n)
+    {
+        printf("Yes\n");
+    }
+    else
+    {
+        printf("No\n");
+    }
     
 
     return 0;
